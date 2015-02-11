21011a12b2dfba617f111a48646c9c7177c2fb44
shukon <shukon@users.noreply.github.com>
2015-02-11 14:18:03 +0100

    Update wikiCrawl.py

--- wikiCrawl.py
+++ wikiCrawl.py
@@ -2,86 +2,53 @@ from urllib.request import urlopen
 import re
 import copy
 
-def crawl(verbose=False):
-    # firstLink = "/Special:Random"
-    # firstLink = "/Port_Macquarie-Hastings_Council" # Randfall <p><span>...
-    # firstLink = "/Schl%C3%B6sser" # Randfall lists (<li>)
-    # firstLink = "/Liste_der_Abk%C3%BCrzungen_antiker_Autoren_und_Werktitel/L"
-    firstLink = "/Mensch"
-    print(firstLink)
-    while (True):
-        url = "http://de.wikipedia.org/wiki" + firstLink
-        firstLink = ''
-        # print(url)
-        code = urlopen(url).read().decode("utf-8")
-        # delete all content in parentheses with preceding [ |>] or trailing [ |,|<]
-        code = re.sub(r'(?<=[ |>]\().+?(?=\)[ |<|,])', '', code)
-        code = re.sub(r'(?<=<p>)<span.+?(?=</span>)', '', code)
-        code = re.sub(r'<td.+?(?=</td>)', '', code, flags=re.DOTALL)
-        # print(code)
-        # first paragraph
-        p = re.compile('(?<=<p>).+')
-        # first list element
-        li = re.compile('(?<=<li>).+')
-        # first td element
-        td = re.compile('(?<=<td).+') # only for strange lists
-        # match links only
-        link = re.compile('(?<=href\="\/wiki)[^:]+?(?=")')
-        # print(p.findall(code)[0])
-        # print(p2.findall(p.findall(code)[0]))
-        listOfParagraphs = p.findall(code)
-        listOfLists = li.findall(code)
-        listOfLinks = []
-        for paragraph in listOfParagraphs:
-            listOfLinks.extend(link.findall(paragraph))
-        # if no link in the paragraphs, look for <li> elements
-        if len(listOfLinks) == 0:
-            for listElement in listOfLists:
-                listOfLinks.extend(link.findall(listElement))
-        firstLink = listOfLinks[0]
-        print(firstLink)
-        if (firstLink.split('/')[::-1][0] == 'Philosophie'):
-            break
-    # if (verbose):
-        # print("Downloaded {} of {} referenced files\n({:.3f} kB in {:.3f} sec)"
-              # .format(counter - failed, counter, totalSize, totalTime))
+# ToDo:
+# Fix problems: - BFS(Spezielle_Zoologie->*)
+#               - getLinks(Adolf_Hitler)
+# Ideas: - Loop Detection
 
 def getLinks(article, noParantheses, includeLI):
     """ Returns a list with all links and titles on wiki article
     in a form of:
     [("link1", "title1"), ("link2", "title2")]
-    do not enter the whole url, just like above
-    noParantheses is boolean and decides whether or not to cut paras
-    includeLI is boolean and decides whether or not to include <li>"""
-    while (True):
-        # print(article)
-        url = "http://de.wikipedia.org/wiki" + article
-        code = urlopen(url).read().decode("utf-8")
-        # delete all content in parentheses with preceding [ |>] or trailing [ |,|<]
-        if noParantheses: code = re.sub(r'(?<=[ |>]\().+?(?=\)[ |<|,])', '', code)
-        code = re.sub(r'(?<=<p>)<span.+?(?=</span>)', '', code)
-        code = re.sub(r'(?<=<div class="NavContent">).+?(?=</div>)', '', code, flags=re.DOTALL)
-        code = re.sub(r'<td.+?(?=</td>)', '', code, flags=re.DOTALL)
-        # first paragraph
-        p = re.compile('(?<=<p>).+?(?=</p>)', flags=re.DOTALL)
-        # first list element
-        li = re.compile('(?<=<li>).+(?=</li>)')
-        # match links and titles only
-        link = re.compile('(?<=href\="\/wiki)([^:]+?)" title="?(.*?)(?=".*</a>)')
-        # go through article
-        listOfParagraphs = p.findall(code)
-        listOfListElements = li.findall(code)
-        listOfLinks = []
-        for paragraph in listOfParagraphs:
-            listOfLinks.extend(link.findall(paragraph))
-        # if no link in the paragraphs, look for <li> elements
-        if includeLI:
-            for listElement in listOfListElements:
-                listOfLinks.extend(link.findall(listElement))
-        return listOfLinks
+    
+    Arguments: article; url, e.g. "/Rieseneidechse"
+               noParantheses; boolean, if True parantheses are cut away (for
+                              example for firstLink
+               includeLI; boolean, if True lists (<li>) are included
+    Returns:   listOfLinks, a list of the form as above           
+               """
+    url = "http://de.wikipedia.org/wiki" + article
+    code = urlopen(url).read().decode("utf-8")
+    # delete all content in parentheses with preceding [ |>] or trailing [ |,|<]
+    #if noParantheses: code = re.sub(r'(?<=[ |>]\().+?(?=\)[ |<|,])?', '', code,
+    if noParantheses: code = re.sub(r'(?<=[ |>]\()[^)]+?(?=\)[ |<|,])', '', code,
+        re.DOTALL)
+    # delete other stuff that you're usually not allowed to use
+    code = re.sub(r'(?<=<p>)<span.+?(?=</span>)', '', code)
+    code = re.sub(r'(?<=<div class="NavContent">).+?(?=</div>)', '', code, flags=re.DOTALL)
+    code = re.sub(r'<td.+?(?=</td>)', '', code, flags=re.DOTALL)
+    # match paragraphs
+    p = re.compile('(?<=<p>).+?(?=</p>)', flags=re.DOTALL)
+    # match list-elements
+    li = re.compile('(?<=<li>).+(?=</li>)')
+    # match links and titles only
+    link = re.compile('(?<=href\="\/wiki)([^:]+?)" title="(.*?)"[>| ]')
+    # split up article
+    listOfParagraphs = p.findall(code)
+    listOfListElements = li.findall(code)
+    # get links from paragraphs
+    listOfLinks = []
+    for paragraph in listOfParagraphs:
+        listOfLinks.extend(link.findall(paragraph))
+    # if no link in the paragraphs, look for <li> elements
+    if includeLI:
+        for listElement in listOfListElements:
+            listOfLinks.extend(link.findall(listElement))
+    return listOfLinks
 
 def firstLink(start):
-    """always "clicks" on first link in article ... (philosophy)"""
+    """always "clicks" on first link in article to find a path to philosophy"""
     if (start.split('/')[::-1][0] == 'Philosophie'):
         print("Philosophie! Hooray!")
     else:
@@ -96,39 +63,37 @@ def firstLink(start):
 def BFS(start, stop):
     """Idea: Follow every way, detect loops, detect end-of-the-lines, detect end
     path is a list of lists, after iterations:
-    [[link1], [link2], [link3]]
+    [[link1], [link2], [link3], ...]
     [[link1, link1.1], [link1, link1.2], [link2, link2.1], [link2, link2.2], ... ]
     ...
     representing the paths. every link consists of (link, title)
     """
-    links = getLinks(start, True, False)
-    paths = []
-    for link in links:
-        paths.append([(start, start), link])
-        if link[0] == stop:
-            print("Path found:")
-            path = []
-            for article in paths[-1]:
-                path.append(article[1])
-            return path
-    # bis hierher richtig
+    paths = [[(start, start)]] # collect all the paths: see above
+    # in loop: pop first path, get all links and add the extended version of the
+    # path for each link to the paths-list
     while(len(paths) != 0):
+        # get links on first path
         path = paths.pop(0)
         links = getLinks(path[-1][0], True, False)
+        # add a new path for each of the links
         for link in links:
             path.append(link)
-            #for el in path:
-            #    print(el[1], end=' -> ')
-            #print()
+            for el in path:
+                print(el[1], end=' -> ')
+            print()
             paths.append(copy.deepcopy(path))
             path.pop(-1)
+            # only executed if target is reached:
             if link[0] == stop:
                 print("Path found:")
                 path = []
                 for article in paths[-1]:
                     path.append(article[1])
                 return path
+    return("You shall not path!")
 
 if __name__ == "__main__":
     # crawl(verbose=True)
-    print(BFS("/Stern", "/Pfeffer"))
+    print(BFS("/Riesensalamander", "/Raufaser")) # FEHLER! (warum?)
+    # print(getLinks("/Adolf_Hitler", True, False)) # FEHLER! (warum?)
+    # print(firstLink("/Schlösser"))
